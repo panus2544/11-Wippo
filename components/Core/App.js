1605cb0e894a72c7272308b02224176dc71c8cec
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-22 03:22:23 +0700

    [UPDATE] create state & wait

--- components/Core/App.js
+++ components/Core/App.js
@@ -2,10 +2,13 @@ import React from 'react'
 import DashBoard from '../DashBoard'
 
 export default class App extends React.Component {
+  state = {
+    allUser:[]
+  }
   render() {
     return (
       <div className="container-fluid">
-        <DashBoard />
+        <DashBoard allUser={this.state.allUser}/>
       </div>
     )
   }
