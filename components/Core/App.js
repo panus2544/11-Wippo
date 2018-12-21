866808b8421b66e10ce51b4d148e07e3e4e6852e
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-22 03:20:46 +0700

    [UPDATE] dashboard

---
+++ components/Core/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashBoard from '../DashBoard'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <DashBoard />
+      </div>
+    )
+  }
+}
