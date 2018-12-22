51f1456096e43c5e81cb2a39d84e932056fb41e1
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-22 18:11:30 +0700

    [UPDATE] add page number & menu

--- components/Core/App.js
+++ components/Core/App.js
@@ -1,27 +1,60 @@
 import React from 'react'
 import DashBoard from '../DashBoard'
-import RegistanceService from '../../service/RegistanceService';
+import Menu from './Menu'
+import Body from './Body'
+import Registant from '../Registants'
+import RegistanceService from '../../service/RegistanceService'
 
 export default class App extends React.Component {
   state = {
-    allUser:[]
+    allUser: [],
+    current: '1',
+    dashboardVisible: 'none',
+    registantVisible: 'none'
   }
-  componentDidMount =()=>{
+  componentDidMount = () => {
     this.getAlluserDB()
-    
+    this.handlePage()
   }
-  getAlluserDB=async()=>{
-   const profiles=await RegistanceService.getAllRegistrant() 
-     this.setState({
+  getAlluserDB = async () => {
+    const profiles = await RegistanceService.getAllRegistrant()
+    this.setState({
       allUser: profiles
-    })   
-  
+    })
   }
-  
+
+  setPage = (props) => {
+    this.setState({
+      current: props
+    })
+    this.handlePage()
+  }
+
+  handlePage = async () => {
+    let { current } = this.state
+    if (current === '1') {
+      this.setState({
+        dashboardVisible: 'block',
+        registantVisible: 'none'
+      })
+    } else if (current === '2') {
+      this.setState({
+        dashboardVisible: 'none',
+        registantVisible: 'block'
+      })
+    }
+  }
+
   render() {
     return (
       <div className="container-fluid">
-        <DashBoard allUser={this.state.allUser}/>
+        <Menu setPage={this.setPage} current={this.state.current} />
+        <Body showComponents={this.state.dashboardVisible}>
+          <DashBoard allUser={this.state.allUser} />
+        </Body>
+        <Body showComponents={this.state.registantVisible}>
+          <Registant />
+        </Body>
       </div>
     )
   }
