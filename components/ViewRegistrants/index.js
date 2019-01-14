87662ad6d086c5eb51e0de193d8517ea24a6d5cd
Mild-TN <mild20091931@gmail.com>
2019-01-14 21:52:27 +0700

    [ADD] สร้างไฟล์ /viewregistrants ที่ pages

--- components/ViewRegistrants/index.js
+++ components/ViewRegistrants/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Table } from 'antd'
+import Nav from '../Core/Navbar'
+import Menu from '../Core/Menu'
 
 
 export default class Registants extends React.Component {
@@ -8,8 +10,21 @@ export default class Registants extends React.Component {
   }
   render() {
     return (
-      <div className="container mt-5">
+      <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <Nav visible={this.state.menu} setPage={this.setPage} current={this.state.current} />
+        </div>
       </div>
+      <div className="row">
+        <div className="col-md-3 col-3">
+          <Menu />
+        </div>
+        <div className="col-md-9 col-9">
+         
+        </div>
+      </div>
+    </div>
     )
   }
 }
