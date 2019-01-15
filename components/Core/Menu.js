9131676eb96c2979a7ecc08f8684eb10e75bb280
Mild-TN <mild20091931@gmail.com>
2019-01-15 23:00:16 +0700

    [UPDATE] แก้ responsive manu

--- components/Core/Menu.js
+++ components/Core/Menu.js
@@ -15,20 +15,19 @@ class Menubar extends React.Component {
   state = {
     showComponent: false,
     collapsed: false,
+    permission : 0
   }
 
   componentDidMount = async () => {
     this.checkPermission()
   }
-
-  checkPermission = async () => {
+  
+  checkPermission = async (permissionIds) => {
     let data = await AuthService.getPermission()
-    console.log(data)
-    if (data.permission[0].permission_id == 1 || data.permission[0].permission_id == 4) {
-      this.setState({
-        showComponent: true
-      })
-    }
+    let permission = await permissionIds
+    if(data.permission[0].permission_id == permission){
+      console.log(permission)
+    }    
   }
 
   onCollapse = (collapsed) => {
@@ -40,20 +39,22 @@ class Menubar extends React.Component {
   render() {
     return (
       <Sider
-        collapsible
-        collapsed={this.state.collapsed}
-        onCollapse={this.onCollapse}
-        theme="light"
+      collapsible
+      collapsed={this.state.collapsed}
+      onCollapse={this.onCollapse}
+      theme="light"
       >
         <div className="logo" />
         <StyleMenu theme="" defaultSelectedKeys={['1']} mode="inline">
-          {this.state.showComponent ?
-            <Menu.Item key="1">
+          {(this.checkPermission(4) && 
+              <Menu.Item key="1">
               <a href='/viewregistrants'>
                 <Icon type="eye" />
                 <span>ดูรายชื่อผู้สมัคร</span>
               </a>
-            </Menu.Item> : ''}
+          </Menu.Item> )
+          }
+
           <Menu.Item key="2">
             <Icon type="eye" />
             <span>Option 1</span>
