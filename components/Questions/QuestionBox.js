aff108adfc5af8dc28273fa50c545e4e9c11c204
Papop Ruengsupapichat <>
2019-01-17 17:31:26 +0700

    [UPDATE] Questions components

--- components/Questions/QuestionBox.js
+++ components/Questions/QuestionBox.js
@@ -2,28 +2,14 @@ import React from "react";
 import RegistanceService from "../../service/RegistanceService";
 
 class QuestionBox extends React.Component {
-  state = {
-    questions: [
-        {id:'',
-    content:''}
-    ]
-  };
-
-  async componentDidMount() {
-    const reqquestions = await RegistanceService.getAllQuestions();
-    this.setState({
-      questions: reqquestions.data
-    });
-    console.log(this.state.questions[0].content);
-  }
-
   render() {
+    
     return (
       <div>
         <div class="card bg-light mb-3" style={{ width: "15vw" }}>
-          <div class="card-header">{this.state.questions[0].id}</div>
+          <div class="card-header">{this.props.questionId}</div>
           <div class="card-body">
-            <p class="card-text">{this.state.questions[0].content}</p>
+            <p class="card-text">{this.props.content}</p>
           </div>
         </div>
       </div>
--- components/Questions/index.js
+++ components/Questions/index.js
@@ -1,19 +1,44 @@
 import React from "react";
 import QuestionBox from "./QuestionBox";
+import RegistanceService from "../../service/RegistanceService";
+
 
 export default class Questions extends React.Component {
-  state = {};
+  state = {
+    questions: [
+      {
+        id: '',
+        content: ''
+      }
+    ]
+  };
+
+  async componentDidMount() {
+    const reqquestions = await RegistanceService.getAllQuestions();
+    this.setState({
+      questions: reqquestions.data
+    });
+    // console.log(this.state.questions[0].content);
+    
+  }
   render() {
+    // console.log(this.state.questions)
     return (
       <div className="container mt-5">
         Hello Questions
         <div className="mt-5">
-          <div class="row">
-
-            <div class="col-4">
-              <QuestionBox />
-            </div>
-            
+          <div className="row">
+            {
+              this.state.questions.map(function (question) {
+                return (
+                  <div className="col-4">
+                    <QuestionBox questionId={question.id} content={question.content}/>
+                  </div>
+                )
+                
+                
+              })
+            }
           </div>
         </div>
       </div>
