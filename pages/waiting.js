fc2c91f061c2326bf56d32f3e3908803ceb37bb8
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-01-18 13:54:05 +0700

    [ADD] route waiting

---
+++ pages/waiting.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Approved from '../components/Approved'
+
+export default class waiting extends React.Component {
+  render() {
+    return (
+      <div>
+        <Approved />
+      </div>
+    )
+  }
+}
