4b8fcab77a41f4ec20acfee916e0acb20adbad6d
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-02-08 16:40:22 +0700

    [ADD] Wait page

---
+++ components/Wait/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card } from 'antd'
+
+export default class index extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-12">
+            <Card title="รอสิทธิ์เข้าถึงระบบ">
+              <p>Test </p>
+            </Card>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
