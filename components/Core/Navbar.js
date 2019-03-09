3f8bc230b45ed711f02a366a5289d61078b571a8
Mild-TN <mild20091931@gmail.com>
2019-03-09 23:00:02 +0700

    [UPDATE] จัด Navbar ใหม่

--- components/Core/Navbar.js
+++ components/Core/Navbar.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { Menu } from 'antd'
+import { Menu, Tooltip, Row, Col } from 'antd'
 import styled from 'styled-components'
 import colors from '../../config/colors'
+import Cookie from '../../service/CookieService'
 
-const StyleMenu = styled(Menu)`
+const StyleMenu = styled.div`
   display: ${props => props.visible};
   background-color : ${colors.navbar};
+  height : 10vh;
+`
+const Nav = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+`
+const Li = styled.li`
+  float: left;
+  padding : 3vh;
+  color: #fff;
+  a {
+    color: #fff;
+    direction : none;
+  }
 `
 
 const MenuItem = Menu.Item;
 class MenuBar extends React.Component {
   state = {
     current: '1',
+    nameFb: '',
+    wipId: 0,
+  }
+
+  componentDidMount = async () => {
+    this.getWipId()
+    this.getName()
   }
 
   handleClick = async (e) => {
@@ -22,22 +46,44 @@ class MenuBar extends React.Component {
     this.props.setPage(changePage)
   }
 
+  getName = async () => {
+    let name = await Cookie.getName()
+    this.setState({
+      nameFb: name,
+    })
+  }
+
+  getWipId = async () => {
+    let wipId = await Cookie.getWipId()
+    this.setState({
+      wipId: wipId
+    })
+  }
+
+  changeTheme = (value) => {
+    this.setState({
+      theme: value ? 'dark' : 'light',
+    });
+  }
+
   render() {
     return (
-      <StyleMenu
-        visible={this.props.visible}
-        onClick={this.handleClick}
-        selectedKeys={[this.props.current]}
-        mode="horizontal"
-        theme='dark'
-      >
-
-        <MenuItem key="1">
-          <a href='/dashboard'>
-            Dash Board
-          </a>
-        </MenuItem>
-      </StyleMenu>
+      <Row >
+        <Col >
+          <StyleMenu>
+            <Nav>
+              <Li>
+                <a href='/dashboard'>
+                  Dash Board
+                </a>
+              </Li>
+              <Li className="float-right">
+                สวัสดี {this.state.nameFb} ( WIP ID : {this.state.wipId} )
+              </Li>
+            </Nav>
+          </StyleMenu>
+        </Col>
+      </Row>
     )
   }
 }
