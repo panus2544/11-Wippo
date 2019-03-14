99ef57e88526998697fc2fd7a1c2e6990652f754
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2019-03-14 13:13:47 +0700

    [ADD] เพิ่ม export Stats ใน RegistrantService

--- service/RegistanceService.js
+++ service/RegistanceService.js
@@ -1,46 +1,63 @@
 import apiRegService from "../utils/apiRegisterService";
-import { async } from "rxjs/internal/scheduler/async";
+import {
+    async
+} from "rxjs/internal/scheduler/async";
 
 const Question = {
-    getAllRegistrant : async ()=>{
-      const respons = await apiRegService.get('/registrants')
+    getAllRegistrant: async () => {
+        const respons = await apiRegService.get('/registrants')
         return respons.data
-    } ,
-    getRegistrantNotRespons : async ()=>{
+    },
+    getRegistrantNotRespons: async () => {
         return await apiRegService.get('/notregistrant')
-    } , 
-    getAllQuestions : async ()=>{
+    },
+    getAllQuestions: async () => {
         return await apiRegService.get('/questions')
-    } ,
-    getAnswersByQuestionId : async (question_id)=>{
+    },
+    getAnswersByQuestionId: async (question_id) => {
         return await apiRegService.get(`/answers/${question_id}`)
     },
-    postAnswerEvaluations : async (answerEva) => {
-        return await apiRegService.post(`/answers/evaluations`,answerEva)
+    postAnswerEvaluations: async (answerEva) => {
+        return await apiRegService.post(`/answers/evaluations`, answerEva)
     },
-    getQuestionById : async (question_id)=>{
+    getQuestionById: async (question_id) => {
         return await apiRegService.get(`/questions/${question_id}`)
     },
-    getDataForChangeStatus : async (data)=>{
-        try{
-            await apiRegService.put('/registrants/changstatus',{
-                'itim_wip_id':data.wipId,
-                'is_call' : data.is_call,
+    getDataForChangeStatus: async (data) => {
+        try {
+            await apiRegService.put('/registrants/changstatus', {
+                'itim_wip_id': data.wipId,
+                'is_call': data.is_call,
             })
-        }catch (error){
+        } catch (error) {
             console.log(error)
         }
-      } ,
-      getDataForUpdateNote : async (data)=>{
-        try{
-            await apiRegService.put('/registrants/note',{
-                'itim_wip_id':data.wipId,
-                'note' : data.note,
+    },
+    getDataForUpdateNote: async (data) => {
+        try {
+            await apiRegService.put('/registrants/note', {
+                'itim_wip_id': data.wipId,
+                'note': data.note,
             })
-        }catch (error){
+        } catch (error) {
             console.log(error)
         }
-      } ,
-     
+    },
+
 }
-export default Question;
+
+export const Stats = {
+    getRegistrantStats: async () => {
+
+        return await apiRegService.get('/stats/registrants');
+    },
+    getRegistrantStatsByDate: async (startdate,enddate) => {
+        return await apiRegService.get('/stats/registrants/date?startdate='+startdate+"&&enddate="+enddate);
+    },
+    getRegistrantStatsByTime: async () => {
+        return await apiRegService.get('/stats/registrants/time');
+    }
+
+}
+
+export  default Question;
