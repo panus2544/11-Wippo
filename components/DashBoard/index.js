efab87a2688a659c8d2d18f39dde423bef7abae9
JaysZila <methaporn.jz@mail.kmutt.ac.th>
2019-03-14 13:16:17 +0700

    [ADD] เพิ่ม method get Stats มาจากหลังบ้าน และแสดงผลค่าที่ return

--- components/DashBoard/index.js
+++ components/DashBoard/index.js
@@ -1,25 +1,46 @@
 import React from 'react'
 import { Card } from 'antd'
+import { Stats } from '../../service/RegistanceService'
 
 export default class DashBoard extends React.Component {
+  state = {
+    stats: [
+      {
+        total_applicant: '',
+        total_success: '',
+        total_unsuccess: ''
+      }
+    ]
+  };
+  async componentDidMount() {
+    this.getCountRegistrants()
+  }
+
+  getCountRegistrants = async () => {
+    const count = await Stats.getRegistrantStats();
+    this.setState({
+      stats: count.data
+    })
+  }
+
   render() {
     return (
-        <div className="row mt-5 p-2">
-          <div className="col col-md-3">
-            <Card title="จำนวนคนที่สมัคร">
-              <h1>{this.props.allUser.length}</h1>
-            </Card>
-          </div>
-          <div className="col col-md-3 mt-2">
-            <Card title="จำนวนคนที่ตอบคำถามเสร็จแล้ว">
-            <h1>{this.props.allUser.length}</h1>
-            </Card>
-          </div>
-          <div className="col col-md-3 mt-2">
-            <Card title="จำนวนคนที่ยังตอบคำถามไม่เสร็จ">
-            <h1>{this.props.allUser.length}</h1>
-            </Card>
-          </div>
+      <div className="row md-12 p-4">
+        <div className="col col-md-4 mt-2">
+          <Card title="จำนวนคนที่สมัคร">
+            {this.state.stats.total_applicant}
+          </Card>
+        </div>
+        <div className="col col-md-4 mt-2">
+          <Card title="จำนวนคนที่ตอบคำถามเสร็จแล้ว">
+            {this.state.stats.total_success}
+          </Card>
+        </div>
+        <div className="col col-md-4 mt-2">
+          <Card title="จำนวนคนที่ยังตอบคำถามไม่เสร็จ">
+            {this.state.stats.total_unsuccess}
+          </Card>
+        </div>
       </div>
     )
   }
