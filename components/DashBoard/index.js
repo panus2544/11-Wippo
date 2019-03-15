fde1e3cdc91ba0ab224f244b4d3412551b0646a6
Mild-TN <mild20091931@gmail.com>
2019-03-15 17:48:40 +0700

    [UPDATE] comment this.props.allUser

--- components/DashBoard/index.js
+++ components/DashBoard/index.js
@@ -7,17 +7,17 @@ export default class DashBoard extends React.Component {
         <div className="row mt-5 p-2">
           <div className="col col-md-3">
             <Card title="จำนวนคนที่สมัคร">
-              <h1>{this.props.allUser.length}</h1>
+              {/* <h1>{this.props.allUser.length}</h1> */}
             </Card>
           </div>
           <div className="col col-md-3 mt-2">
             <Card title="จำนวนคนที่ตอบคำถามเสร็จแล้ว">
-            <h1>{this.props.allUser.length}</h1>
+            {/* <h1>{this.props.allUser.length}</h1> */}
             </Card>
           </div>
           <div className="col col-md-3 mt-2">
             <Card title="จำนวนคนที่ยังตอบคำถามไม่เสร็จ">
-            <h1>{this.props.allUser.length}</h1>
+            {/* <h1>{this.props.allUser.length}</h1> */}
             </Card>
           </div>
       </div>
