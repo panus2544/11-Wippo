953b67a129ebccb929d58c5bfa7f738899f05bf6
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-03-15 16:37:44 +0700

    [ADD] ขึ้นโครงหน้า ดูโรคประจำตัว + ยาที่แพ้ และ หน้าดูอาหารที่แพ้

---
+++ components/CheckDisease/TableDisease.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Table, Input, Checkbox } from 'antd'
+import Registrants from '../../service/RegistanceService'
+
+class DiseaseTable extends React.Component {
+  state = {
+    selectedRowKeys: [],
+    loading: false,
+    registrants: [],
+    note: '',
+    columns: [
+      {
+        title: 'รายชื่อ',
+        dataIndex: 'name',
+        key: 'wip_id'
+      },
+      {
+        title: 'โรคประจำตัว',
+        dataIndex: '',
+        key: ''
+      },
+      {
+        title: 'ยาที่แพ้',
+        dataIndex: '',
+        key: ''
+      }
+    ]
+  }
+
+  componentDidMount = async () => {
+    let registrants = await Registrants.getAllRegistrant()
+    this.getRegistrant(registrants.registrants)
+  }
+
+  getRegistrant = async registrants => {
+    let data = []
+    for (let index = 0; index < registrants.length; index++) {
+      data.push({
+        key: index,
+        wip_id: registrants[index].wip_id,
+        is_call: registrants[index].is_call,
+        name: `${registrants[index].firstname_th} ${
+          registrants[index].lastname_th
+        }`,
+        tel: registrants[index].telno,
+        message: registrants[index].note
+      })
+    }
+    this.setState({
+      registrants: data
+    })
+  }
+
+  onSelectChange = selectedRowKeys => {
+    this.setState({ selectedRowKeys })
+  }
+
+  handleCheckStatus = (wip_id, e) => {
+    Registrants.getDataForChangeStatus({
+      wipId: wip_id,
+      is_call: e.target.checked
+    })
+  }
+
+  handleUnfocus = (wip_id, e) => {
+    Registrants.getDataForUpdateNote({ wipId: wip_id, note: e.target.value })
+  }
+
+  render() {
+    return (
+      <Table columns={this.state.columns} dataSource={this.state.registrants} />
+    )
+  }
+}
+
+export default DiseaseTable
