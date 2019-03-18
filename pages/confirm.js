e2e29b6655badd23f6e3ef23e0dbf23b24879fe0
Mild-TN <mild20091931@gmail.com>
2019-03-18 14:25:55 +0700

    [ADD] FE - สร้างไฟล์ confirm ใน pages

---
+++ pages/confirm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+function mapStateToProps(state) {
+  return {
+
+  };
+}
+
+class confirm extends Component {
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    );
+  }
+}
+
+export default confirm
