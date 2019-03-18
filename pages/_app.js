ff1fd989fc64268b794a8f54996c23f0bf3654b0
Mild-TN <mild20091931@gmail.com>
2019-03-18 14:47:00 +0700

    [UPDATE] เพิ่ม title

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import Head from 'next/head'
 import 'antd/dist/antd.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 export default class MyApp extends App {
-  static async getInitialProps ({ Component, router, ctx }) {
+  static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {}
 
     if (Component.getInitialProps) {
@@ -15,11 +16,16 @@ export default class MyApp extends App {
     return { pageProps }
   }
 
-  render () {
+  render() {
     const { Component, pageProps } = this.props
 
     return (
       <Container>
+        <Head>
+          <title>
+            ระบบจัดการค่าย Wip Camp #11
+          </title>
+        </Head>
         <Component {...pageProps} />
       </Container>
     )
