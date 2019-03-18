4abeaf03ee9f347680aaa5e84489d2ae9dd68248
Mild-TN <mild20091931@gmail.com>
2019-03-18 15:48:28 +0700

    [UPDATE] FE- import Navbar และ menu

--- components/ConfirmName/index.js
+++ components/ConfirmName/index.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import Nav from '../Core/Navbar'
+import Menu from '../Core/Menu'
+import styled from 'styled-components'
+
+const ZIndex = styled.div`
+  z-index: 10;
+`
 
 class index extends Component {
   constructor(props) {
@@ -13,9 +20,19 @@ class index extends Component {
 
   render() {
     return (
-      <div>
-        Hello world !!
-      </div>
+      <div className="container-fulid overflow-hidden">
+        <div className="row">
+          <div className="col-12 col-md-12 ">
+            <Nav  />
+          </div>
+          <ZIndex className="col-3 col-md-2">
+            <Menu/>
+          </ZIndex>
+          <div className="col-9 col-md-10 p-5" >
+            {/* <Table/> */}
+          </div>
+        </div>
+      </div >
     );
   }
 }
