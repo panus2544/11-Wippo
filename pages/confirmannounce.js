a5a1c66929734abe2eba4fd62a2e2b7c7e1d5302
Mild-TN <mild20091931@gmail.com>
2019-03-29 22:54:20 +0700

    [ADD] สร้าง confirmannounce.js ใน page

---
+++ pages/confirmannounce.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import App from '../components/ConfirmAnnounce'
+
+class confirmannounce extends Component {
+  render() {
+    return (
+      <App/>
+    );
+  }
+}
+
+export default confirmannounce;
