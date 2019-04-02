1f5096a7067a729e59059b90c4320fdc3c607059
Mild-TN <mild20091931@gmail.com>
2019-04-02 22:57:32 +0700

    [ADD] checkdocument.js

---
+++ pages/checkdocument.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import App from '../components/CheckDocument'
+
+class checkdocument extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+export default checkdocument;
