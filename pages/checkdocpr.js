91d00dd13f5fc526840cc6f82c972baccd095af3
Mild-TN <mild20091931@gmail.com>
2019-04-03 12:47:13 +0700

    [ADD] Create checkdocpr.js

---
+++ pages/checkdocpr.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import App from '../components/CheckDocumentPr'
+
+class checkdocument extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+export default checkdocument;
