b57fd73362ae1d8e74061fec5309f5a4d119e5a2
Mild-TN <mild20091931@gmail.com>
2019-04-03 10:58:52 +0700

    [UPDATE] count success and unsecces

--- components/CheckDocument/TableCheck.js
+++ components/CheckDocument/TableCheck.js
@@ -16,60 +16,69 @@ class TableCheck extends Component {
     registrants: [],
     url: null,
     note: '',
-    columns: [{
-      title: 'ตรวจแล้ว',
-      dataIndex: 'checked',
-      render: (boolean, profile) => {
-        return (
-          boolean == 'checked' ?
-            <Checkbox defaultChecked={true} onChange={(e) => this.handleCheckStatus(profile.wip_id, e)} /> :
-            <Checkbox defaultChecked={false} onChange={(e) => this.handleCheckStatus(profile.wip_id, e)} />
-        )
-      }
-    }, {
-      title: 'WIP ID',
-      dataIndex: 'wip_id',
-      key: 'wip_id'
-    }, {
-      title: 'ชื่อ-สกุล',
-      dataIndex: 'name',
-    }, {
-      title: 'Transcript',
-      dataIndex: 'transcript',
-      render: (link) => {
-        return (
-          link != null ?
-            <a onClick={() => this.getDocument(link)}>
-              <P color="#1aa1f4" >ตรวจเอกสาร</P>
-            </a> :
-            <P opacity="0.3">ยังไม่อัพโหลด</P>
-        )
-      }
-    }, {
-      title: 'ใบขออนุญาต ผปค.',
-      dataIndex: 'confrim',
-      render: (link) => {
-        return (
-          link != null ?
-            <a onClick={() => this.getDocument(link)}>
-              <P color="#1aa1f4">ตรวจเอกสาร</P>
-            </a> :
-            <P opacity="0.3">ยังไม่อัพโหลด</P>
-        )
-      }
-    }, , {
-      title: 'Receipt',
-      dataIndex: 'receipt',
-      render: (link) => {
-        return (
-          link != null ?
-            <a onClick={() => this.getDocument(link)}>
-              <P color="#1aa1f4">ตรวจเอกสาร</P>
-            </a> :
-            <P opacity="0.3">ยังไม่อัพโหลด</P>
-        )
-      }
-    }]
+    countUnsucces : 0,
+    countSucces : 0,
+    columns: [
+      {
+        title: 'ลำดับ',
+        dataIndex: 'count',
+      }, {
+        title: 'ตรวจแล้ว',
+        dataIndex: 'checked',
+        render: (boolean, profile) => {
+          return (
+            boolean == 'checked' ?
+              <Checkbox defaultChecked={true} onChange={(e) => this.handleCheckStatus(profile.wip_id, e)} /> :
+              <Checkbox defaultChecked={false} onChange={(e) => this.handleCheckStatus(profile.wip_id, e)} />
+          )
+        }
+      }, {
+        title: 'WIP ID',
+        dataIndex: 'wip_id',
+        key: 'wip_id'
+      }, {
+        title: 'ชื่อ-สกุล',
+        dataIndex: 'name',
+      }, {
+        title: 'เบอร์โทร',
+        dataIndex: 'tel',
+      }, {
+        title: 'Transcript',
+        dataIndex: 'transcript',
+        render: (link) => {
+          return (
+            link != null ?
+              <a onClick={() => this.getDocument(link)}>
+                <P color="#1aa1f4" >ตรวจเอกสาร</P>
+              </a> :
+              <P opacity="0.3">ยังไม่อัพโหลด</P>
+          )
+        }
+      }, {
+        title: 'ใบขออนุญาต ผปค.',
+        dataIndex: 'confrim',
+        render: (link) => {
+          return (
+            link != null ?
+              <a onClick={() => this.getDocument(link)}>
+                <P color="#1aa1f4">ตรวจเอกสาร</P>
+              </a> :
+              <P opacity="0.3">ยังไม่อัพโหลด</P>
+          )
+        }
+      }, , {
+        title: 'Receipt',
+        dataIndex: 'receipt',
+        render: (link) => {
+          return (
+            link != null ?
+              <a onClick={() => this.getDocument(link)}>
+                <P color="#1aa1f4">ตรวจเอกสาร</P>
+              </a> :
+              <P opacity="0.3">ยังไม่อัพโหลด</P>
+          )
+        }
+      }]
   }
 
   componentDidMount = async () => {
@@ -96,19 +105,30 @@ class TableCheck extends Component {
 
   getCamper = async camper => {
     let data = [];
+    let countSucces = 0;
+    let countUnsucces = 0;
     for (let index = 0; index < camper.length; index++) {
       data.push({
         key: index,
+        count: index + 1,
         wip_id: camper[index].wip_id,
         checked: camper[index].reason,
         name: `${camper[index].firstname_th} ${camper[index].lastname_th}`,
+        tel: camper[index].telno,
         transcript: camper[index].transcript,
         confrim: camper[index].confrim,
         receipt: camper[index].receipt,
       })
+      if (camper[index].status == 'success') {
+        countSucces = countSucces + 1
+      }else {
+        countUnsucces = countUnsucces + 1
+      }
     }
     this.setState({
-      registrants: data
+      registrants: data,
+      countSucces : countSucces,
+      countUnsucces : countUnsucces
     })
   }
 
@@ -134,7 +154,10 @@ class TableCheck extends Component {
   render() {
     return (
       <React.Fragment>
-        <Table columns={this.state.columns} dataSource={this.state.registrants} />
+        <div className='d-flex justify-content-end'>
+          อัพโหลดแต่ยังไม่ยืนยันสิทธิ์ : {this.state.countUnsucces}  ยืนยันสิทธิ์แล้ว : {this.state.countSucces} 
+        </div>
+          <Table columns={this.state.columns} dataSource={this.state.registrants} />
       </React.Fragment>
     );
   }
