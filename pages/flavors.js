c1fe0bb6c807cffe42e185ca2119eae64d57b384
Mild-TN <mild20091931@gmail.com>
2019-04-19 23:43:35 +0700

    [ADD] flavors.js

---
+++ pages/flavors.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Flavors from '../components/Flavors'
+
+class flavors extends Component {
+  render() {
+    return (
+      <Flavors/>
+    );
+  }
+}
+
+export default flavors;
